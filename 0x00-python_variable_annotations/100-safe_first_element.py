9a73256c8ed409f3590920ee91ecda9eac581cf6
Biniyam <biniyamkiya3682@gmail.com>
2023-07-06 18:31:24 +0300

    Python Variable

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Task 10"""
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Retrieves the first element"""
+    if lst:
+        return lst[0]
+    else:
+        return None
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Task 2"""
+
+
+def floor(a: float) -> int:
+    """Computes the floor of a floating"""
+    return int(a)
